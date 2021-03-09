3d8e11f19054e73e9b3b937fac8ed0a7fc0c5cdd
Ekramul Hassan <78520987+mao2116@users.noreply.github.com>
2021-03-09 12:03:52 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
+from pyfiglet import figlet_format
+from termcolor import colored
+import sys,time
+x = 'mao2116'
+x = figlet_format(x, 'slant')
+x = colored(x, 'cyan')
 
+def EH(z):
+  for ww in z + '\n':
+    sys.stdout.write(ww)
+    sys.stdout.flush()
+    time.sleep(0.01)
+EH(x)
